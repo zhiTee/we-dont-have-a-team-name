9ae3b07744de4738bf624ee5f90c3d6115bd8f57
Jason Tee <ztee0007@student.monash.edu>
2025-09-20 13:38:12 +0800

    Change the codebase for the front-end

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
 
 export default function Home() {
   return (
-    // https://youtu.be/YKpA0JG8wTk?si=4ZF9_Ys1j1v7Z9M-     use this :)
-    // UI design taken from : https://ui.shadcn.com/docs/installation/next
-    
-    <div>
-      <Button>Testing button</Button>
-    </div>
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <h1>Hello!</h1>
+
+      <Input type="email" placeholder="Email" />
         
+    </div>
   );
 }
