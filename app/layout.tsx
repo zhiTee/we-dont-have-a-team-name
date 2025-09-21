84e5ca3c805cc4a0b49e182ef6997c53fb092e45
jlim0255 <jlim0255.work@gmail.com>
2025-09-21 20:35:21 +0800

    fixed hydration issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         style={{ backgroundColor: '#faf5eaff' }}
+        suppressHydrationWarning={true}
       >
         <Navbar />
         {children}
