44c3d48cb9970e797ca8ea654916f0e4565ba153
Jason Tee <ztee0007@student.monash.edu>
2025-09-22 00:37:06 +0800

    Modify code for amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "donthaveateamname": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
