8135235970fe30b156f34054e37565cf180c59b9
Jason Tee <ztee0007@student.monash.edu>
2025-09-22 00:27:47 +0800

    Add more code for amplify

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,32 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam';
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
-  auth,
+export const backend = defineBackend({
   data,
+});
+
+// Add custom IAM policies for AWS services
+backend.addOutput({
+  custom: {
+    bedrockPolicy: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Effect: 'Allow',
+          Action: [
+            'bedrock:InvokeModel',
+            'bedrock:RetrieveAndGenerate',
+            'bedrock:Retrieve',
+            's3:GetObject',
+            's3:PutObject',
+            'textract:AnalyzeDocument',
+            'transcribe:StartTranscriptionJob',
+            'transcribe:GetTranscriptionJob'
+          ],
+          Resource: '*'
+        }
+      ]
+    }
+  }
 });
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "donthaveateamname": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
